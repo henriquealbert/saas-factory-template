e2818cacee4df526ffdc40338d4fc3e65b7163e6
Henrique Schmaiske <ishenriquealbert@gmail.com>
2025-02-09 19:50:34 -0300

    Add AdonisJS Mail commands and provider to application configuration

--- adonisrc.ts
+++ adonisrc.ts
@@ -15,6 +15,7 @@ export default defineConfig({
     () => import('@adonisjs/lucid/commands'),
     () => import('@adocasts.com/dto/commands'),
     () => import('@adocasts.com/actions/commands'),
+    () => import('@adonisjs/mail/commands'),
   ],
 
   /*
@@ -44,6 +45,7 @@ export default defineConfig({
     () => import('@adonisjs/auth/auth_provider'),
     () => import('@adonisjs/inertia/inertia_provider'),
     () => import('@adonisjs/limiter/limiter_provider'),
+    () => import('@adonisjs/mail/mail_provider'),
   ],
 
   /*

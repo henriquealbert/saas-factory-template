806a8cc5d45477512f55f6384445e65f49587af8
Henrique Schmaiske <ishenriquealbert@gmail.com>
2025-02-09 19:26:09 -0300

    Add logout controller for handling user logout

---
+++ app/controllers/auth/logout_controller.ts
@@ -0,0 +1,12 @@
+import WebLogout from '#actions/auth/web_logout'
+import { inject } from '@adonisjs/core'
+import type { HttpContext } from '@adonisjs/core/http'
+
+export default class LogoutController {
+  @inject()
+  async handle({ response }: HttpContext, webLogout: WebLogout) {
+    await webLogout.handle()
+
+    return response.redirect().toRoute('login.show')
+  }
+}

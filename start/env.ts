d8eccaa2fb21c262282a9131a48daceca581cdb8
Henrique Schmaiske <ishenriquealbert@gmail.com>
2025-02-09 19:29:38 -0300

    Configure rate limiting with database and memory stores

--- start/env.ts
+++ start/env.ts
@@ -35,4 +35,11 @@ export default await Env.create(new URL('../', import.meta.url), {
   DB_USER: Env.schema.string(),
   DB_PASSWORD: Env.schema.string.optional(),
   DB_DATABASE: Env.schema.string(),
+
+  /*
+  |----------------------------------------------------------
+  | Variables for configuring the limiter package
+  |----------------------------------------------------------
+  */
+  LIMITER_STORE: Env.schema.enum(['database', 'memory'] as const),
 })

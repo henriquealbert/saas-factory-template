0fbf68816b4f522b16889ce76ceb91dfde4b066c
Henrique Schmaiske <ishenriquealbert@gmail.com>
2025-02-09 19:14:20 -0300

    Add user relationships for password reset tokens and organizations

--- app/models/user.ts
+++ app/models/user.ts
@@ -1,10 +1,13 @@
 import { DateTime } from 'luxon'
 import hash from '@adonisjs/core/services/hash'
 import { compose } from '@adonisjs/core/helpers'
-import { BaseModel, column } from '@adonisjs/lucid/orm'
+import { BaseModel, column, hasMany, manyToMany } from '@adonisjs/lucid/orm'
 import { withAuthFinder } from '@adonisjs/auth/mixins/lucid'
-import { DbAccessTokensProvider } from '@adonisjs/auth/access_tokens'
+import type { HasMany, ManyToMany } from '@adonisjs/lucid/types/relations'
+import PasswordResetToken from './password_reset_token.js'
+import Organization from './organization.js'
 import { DbRememberMeTokensProvider } from '@adonisjs/auth/session'
+import { DbAccessTokensProvider } from '@adonisjs/auth/access_tokens'
 
 const AuthFinder = withAuthFinder(() => hash.use('scrypt'), {
   uids: ['email'],
@@ -12,6 +15,9 @@ const AuthFinder = withAuthFinder(() => hash.use('scrypt'), {
 })
 
 export default class User extends compose(BaseModel, AuthFinder) {
+  static accessTokens = DbAccessTokensProvider.forModel(User)
+  static rememberMeTokens = DbRememberMeTokensProvider.forModel(User)
+
   @column({ isPrimary: true })
   declare id: number
 
@@ -30,6 +36,12 @@ export default class User extends compose(BaseModel, AuthFinder) {
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   declare updatedAt: DateTime | null
 
-  static accessTokens = DbAccessTokensProvider.forModel(User)
-  static rememberMeTokens = DbRememberMeTokensProvider.forModel(User)
+  @hasMany(() => PasswordResetToken)
+  declare passwordResetTokens: HasMany<typeof PasswordResetToken>
+
+  @manyToMany(() => Organization, {
+    pivotTable: 'organization_users',
+    pivotColumns: ['role_id'],
+  })
+  declare organizations: ManyToMany<typeof Organization>
 }

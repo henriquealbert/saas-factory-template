1982a08e6eda2fed65ec390b50c322460f21dd36
Henrique Schmaiske <ishenriquealbert@gmail.com>
2025-02-09 19:14:43 -0300

    Add OrganizationInvite model with relationships and attributes

---
+++ app/models/organization_invite.ts
@@ -0,0 +1,54 @@
+import { DateTime } from 'luxon'
+import { BaseModel, belongsTo, column } from '@adonisjs/lucid/orm'
+import type { BelongsTo } from '@adonisjs/lucid/types/relations'
+import User from './user.js'
+import Organization from './organization.js'
+import Role from './role.js'
+
+export default class OrganizationInvite extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column()
+  declare invitedByUserId: number
+
+  @column()
+  declare canceledByUserId: number | null
+
+  @column()
+  declare roleId: number
+
+  @column()
+  declare email: string
+
+  @column.dateTime()
+  declare acceptedAt: DateTime | null
+
+  @column.dateTime()
+  declare canceledAt: DateTime | null
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+
+  @belongsTo(() => User, {
+    foreignKey: 'invitedByUserId',
+  })
+  declare invitedByUser: BelongsTo<typeof User>
+
+  @belongsTo(() => User, {
+    foreignKey: 'canceledByUserId',
+  })
+  declare canceledByUser: BelongsTo<typeof User>
+
+  @belongsTo(() => Role)
+  declare role: BelongsTo<typeof Role>
+
+  @column()
+  declare organizationId: number
+
+  @belongsTo(() => Organization)
+  declare organization: BelongsTo<typeof Organization>
+}

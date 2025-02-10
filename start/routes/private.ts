7421ff527c45513996504e4a65dbd27dc595c367
Henrique Schmaiske <ishenriquealbert@gmail.com>
2025-02-10 09:36:10 -0300

    Add commented settings routes for profile, account, and organization management

--- start/routes/private.ts
+++ start/routes/private.ts
@@ -15,3 +15,44 @@ router
       .as('organizations.destroy')
   })
   .use(middleware.auth())
+
+// @TODO: Add settings routes
+// router
+//   .group(() => {
+//     //* Settings (Profile)
+//     router
+//       .get('/settings/profile', [SettingsProfilesController, 'index'])
+//       .as('settings.profile.index')
+//     router
+//       .put('/settings/profile', [SettingsProfilesController, 'update'])
+//       .as('settings.profile.update')
+
+//     //* Settings (Account)
+//     router
+//       .get('/settings/account', [SettingsAccountsController, 'index'])
+//       .as('settings.account.index')
+//     router
+//       .put('/settings/account/email', [SettingsAccountsController, 'updateEmail'])
+//       .as('settings.account.email')
+//     router
+//       .delete('/settings/account', [SettingsAccountsController, 'destroy'])
+//       .as('settings.account.destroy')
+
+//     //* Settings (Organization)
+//     router
+//       .get('/settings/organization', [SettingsOrganizationsController, 'index'])
+//       .as('settings.organization.index')
+//     router
+//       .post('/settings/organization/invite', [SettingsOrganizationsController, 'inviteUser'])
+//       .as('settings.organization.invite')
+//     router
+//       .delete('/settings/organization/invite/:id', [
+//         SettingsOrganizationsController,
+//         'cancelInvite',
+//       ])
+//       .as('settings.organization.invite.cancel')
+//     router
+//       .delete('/settings/organization/user/:id', [SettingsOrganizationsController, 'removeUser'])
+//       .as('settings.organization.user.remove')
+//   })
+//   .use([middleware.auth(), middleware.organization()])

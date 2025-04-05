a0cab9f6e6558e8c416b0cea5006496d406062b7
Henrique Albert Schmaiske <ishenriquealbert@gmail.com>
2025-04-05 10:52:13 -0300

    Remove app.css and update StylesProvider to use new theme and CSS variable resolver

--- inertia/styles/styles_provider.tsx
+++ inertia/styles/styles_provider.tsx
@@ -1,9 +1,15 @@
 // All packages except `@mantine/hooks` require styles imports
 import '@mantine/core/styles.css'
-import '../styles/app.css'
 
 import { MantineProvider } from '@mantine/core'
+import { shadcnCssVariableResolver } from './css_variable_resolver'
+import { shadcnTheme } from './theme'
+import './style.css'
 
 export function StylesProvider({ children }: { children: React.ReactNode }) {
-  return <MantineProvider>{children}</MantineProvider>
+  return (
+    <MantineProvider theme={shadcnTheme} cssVariablesResolver={shadcnCssVariableResolver}>
+      {children}
+    </MantineProvider>
+  )
 }

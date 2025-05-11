3e7a42f3cfbc942b39ec01b11c225676fa6a184b
Henrique Albert Schmaiske <ishenriquealbert@gmail.com>
2025-05-11 15:35:26 -0300

    `fix(auth): redirect to home after password reset`

--- app/controllers/auth/forgot_passwords_controller.ts
+++ app/controllers/auth/forgot_passwords_controller.ts
@@ -49,6 +49,6 @@ export default class ForgotPasswordsController {
 
     session.flash('success', 'Your password has been updated')
 
-    return response.redirect().toRoute('login.show')
+    return response.redirect().toRoute('home.index')
   }
 }
